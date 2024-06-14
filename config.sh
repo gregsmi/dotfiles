c07f54859bfe0551c46debbe2a5d4fbf3c0789a3
Greg Smith <gregsmi@microsoft.com>
2024-06-14 09:44:59 -0700

    idempotent script

--- config.sh
+++ config.sh
@@ -1,7 +1,16 @@
 # curl -s https://raw.githubusercontent.com/gregsmi/dotfiles/main/config.sh | bash
 cd ~
-git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
-git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+
+# Install powerlevel10k if it doesn't exist.
+if [ ! -d "${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k" ]; then
+    git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
+fi
+# Install zsh-autosuggestions if it doesn't exist.
+if [ ! -d "${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/plugins/zsh-autosuggestions" ]; then
+    git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+fi
+
+# Copy config files.
 mv .zshrc .zshrc.bak
 curl 'https://raw.githubusercontent.com/gregsmi/dotfiles/main/.p10k.zsh' > .p10k.zsh
 curl 'https://raw.githubusercontent.com/gregsmi/dotfiles/main/.zshrc' > .zshrc
