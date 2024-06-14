f99151f0962a28eb74742acd278d14848a5b321f
Greg Smith <gregsmi@microsoft.com>
2024-06-14 09:51:13 -0700

    update

--- config.sh
+++ config.sh
@@ -1,13 +1,14 @@
 # curl -s https://raw.githubusercontent.com/gregsmi/dotfiles/main/config.sh | bash
 cd ~
 
+CUSTOM_DIR="${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}"
 # Install powerlevel10k if it doesn't exist.
-if [ ! -d "${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k" ]; then
-    git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
+if [ ! -d "$CUSTOM_DIR/themes/powerlevel10k" ]; then
+    git clone --depth=1 https://github.com/romkatv/powerlevel10k.git $CUSTOM_DIR/themes/powerlevel10k
 fi
 # Install zsh-autosuggestions if it doesn't exist.
-if [ ! -d "${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/plugins/zsh-autosuggestions" ]; then
-    git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+if [ ! -d "$CUSTOM_DIR/plugins/zsh-autosuggestions" ]; then
+    git clone https://github.com/zsh-users/zsh-autosuggestions $CUSTOM_DIR/plugins/zsh-autosuggestions
 fi
 
 # Copy config files.
